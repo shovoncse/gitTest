10b10ed054348ab95f8a81a6c3968480ea7c01f8
shovondas <shovondas26@gmail.com>
2018-11-09 23:03:42 +0600

    Change greeting method

--- hello.java
+++ hello.java
@@ -2,11 +2,14 @@ public class hello {
     public static void main(String[] args){
         System.out.println("VS Code Is Awesome");    
         
-        greetings("Shovon Das");
+        // greetings("Shovon Das");
+        greetings("git");
         }
 
     
 public static void greetings(String[] name){
-    System.out.println("Welcome " + name);
+    System.out.println("Bye " + name);
 }
+
+// testing perpose
 }
