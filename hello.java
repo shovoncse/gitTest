c81ed6329d4ff6eed5be9ccbaade98d2fcda45c5
shovondas <shovondas26@gmail.com>
2018-11-10 00:07:47 +0600

    removed a lot

--- hello.java
+++ hello.java
@@ -1,21 +1,9 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS Code Is Awesome");    
-        
-        // greetings("Shovon Das");
-        greetings("git");
-        name("HM");
         }
 
-    
-public static void greetings(String[] name){
-    System.out.println("Bye " + name);
-}
-
-// testing perpose
-
-void show(string name){
-    System.out.println(name);
-}
-
+        public static void message() {
+            System.out.println("Now we are able to contribute Open Source....");
+        }
 }
