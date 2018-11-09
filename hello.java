ca6a58168c9433835480b1a6a7752f46f40caa32
shovondas <shovondas26@gmail.com>
2018-11-09 21:41:38 +0600

    Added a new method called greetings to hello.,java

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("VS Code Is Awesome");    
+        
+        greetings("Shovon Das");
         }
+
+    
+public static void greetings(String[] name){
+    System.out.println("Welcome " + name);
+}
 }
